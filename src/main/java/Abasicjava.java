a035455b52bd8db79ce719fbd321f4ea4da9eea5
anandaniket111-cell <anandaniket111@gmail.com>
2025-09-28 11:19:46 +0530

    i write new code
    how to create class and packeges

---
+++ src/main/java/Abasicjava.java
@@ -0,0 +1,6 @@
+public class Abasicjava {
+    public static void main(String[] args) {
+        System.out.println("my name is aniket");
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/java_days/day1.java
@@ -0,0 +1,10 @@
+package java_days;
+
+public class day1 {
+
+    public static void main(String[] args) {
+
+        String a="aniket kolekar";
+        System.out.println(a);
+    }
+}
